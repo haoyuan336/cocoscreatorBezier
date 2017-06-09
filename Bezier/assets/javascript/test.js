7036c87465bd9e8c6f7843e5f42a49506c1806c4
楚浩远 <328387458@qq.com>
2017-06-09 18:08:03 +0800

    多条曲线的操作

--- Bezier/assets/javascript/test.js
+++ Bezier/assets/javascript/test.js
@@ -20,32 +20,62 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        let p1 = {x: 100,y: 200};
-        let p2 = {x: 300,y: 400};
+        let pos1 = {x: 100,y: 200};
+        let pos2 = {x: 300,y: 400};
+
+        let pos3 = {x: 500,y: 800};
         ///y = kx + b
-        let k = (p2.y - p1.y)/(p2.x - p1.x);
-        let b = (p1.y * p2.x - p2.y * p1.x) / (p2.x - p1.x);
-        console.log('k = ' + k);
-        console.log('b = ' + b);
+        // let k = (p2.y - p1.y)/(p2.x - p1.x);
+        // let b = (p1.y * p2.x - p2.y * p1.x) / (p2.x - p1.x);
+        //
+        // console.log('k = ' + k);
+        // console.log('b = ' + b);
 
-
+        const getSun = function (p1,p2) {
+            return {
+                k: (p2.y - p1.y)/(p2.x - p1.x),
+                b: (p1.y * p2.x - p2.y * p1.x) / (p2.x - p1.x),
+                range: {
+                    p1:p1,
+                    p2:p2
+                }
+            }
+        };
+        let sun1 = getSun(pos1,pos2);
+        let sun2 = getSun(pos2,pos3);
+        let sunList = [];
+        sunList.push(sun1,sun2);
 
         const getX = function (y) {
-            return (y - b) / k;
+            for (let i = 0 ; i < sunList.length ; i ++){
+                if (y > sunList[i].range.p1.y && y < sunList[i].range.p2.y){
+                    return  ( y - sunList[i].b ) / sunList[i].k;
+                }
+            }
+            // return (y - b) / k;
         };
         const getY = function (x) {
-          return k * x + b;
+            // let sun1 = getSun(pos1,pos2);
+            // let sun2 = getSun(pos2,pos3);
+            // return kx + b;
+            for (let i = 0 ; i < sunList.length ; i ++){
+                if (x > sunList[i].range.p1.x && x < sunList[i].range.p2.x){
+                    return  x * sunList[i].k + sunList[i].b;
+                }
+            }
         };
 
         this.Graphics = this.node.getComponent(cc.Graphics);
         this.Graphics.strokeColor=cc.Color.RED;
-        this.Graphics.lineWidth=1;
-        this.Graphics.moveTo(p1.x,p1.y);
-        this.Graphics.lineTo(p2.x,p2.y);
-        this.Graphics.fill();
+        this.Graphics.lineWidth=10;
+        this.Graphics.moveTo(pos1.x,pos1.y);
+        this.Graphics.lineTo(pos2.x,pos2.y);
+        this.Graphics.lineTo(pos3.x,pos3.y);
+
+        // this.Graphics.fill();
         this.Graphics.stroke();
         
-        this.Bollean.position = p1;
+        this.Bollean.position = pos1;
         
 
         
